d7e3f83e30a2736e697fefeb6b2658912e92e1f4
zouhangte <zouhantezbm@126.com>
2018-04-10 10:01:17 +0800

    导入2.0版本的代码读取工具

---
+++ Assets/Demo/Editor/AutoControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System;
+
+public class AutoControl
+{
+    public static MenuCommand Command { get; private set; }
+
+    [InitializeOnLoadMethod]
+    public static void Regist()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindow;
+    }
+
+    private static void OnHierarchyWindow(int instanceID, Rect selectionRect)
+    {
+        if (Event.current != null && selectionRect.Contains(Event.current.mousePosition)
+           && Event.current.button == 1 && Event.current.type <= EventType.mouseUp)
+        {
+            Debug.Log(EditorUtility.InstanceIDToObject(instanceID));
+            EditorUtility.DisplayPopupMenu(selectionRect, "Assets", Command);
+        }
+    }
+}
